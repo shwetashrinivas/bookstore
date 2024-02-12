f494d6e2c707e589ace68418242271957223ecdd
santosh.routray <santosh.routray@gmail.com>
2024-02-12 15:09:38 +0530

    add migration for books table

--- src/main/resources/db/migration/schema/V2__Update_Book.sql
+++ src/main/resources/db/migration/schema/V2__Update_Book.sql
@@ -1,10 +1,10 @@
 alter table books
-add author varchar(255) not null
-add price varchar(255) not null
-add description varchar(255) not null,
-    coverImageUrl varchar(255),
-    rating decimal not null,
-    quanity int not null,
-    createdAt TIMESTAMP,
-    updatedAt TIMESTAMP,
+    add column author varchar(255) not null,
+    add column price decimal not null,
+    add column description text not null,
+    add column  coverImageUrl varchar(255),
+    add column rating decimal not null,
+    add  column quanity int not null,
+    add  column createdAt TIMESTAMP,
+    add column updatedAt TIMESTAMP;
 
