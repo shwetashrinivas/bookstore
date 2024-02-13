505bfb44229f3232f8ca29b5cb71aaf3b514942b
Badhmanaban Madhu <badhmanaban.madhu@idfcbank.com>
2024-02-13 12:58:13 +0530

    resolve cross origin in spring boot app

--- src/main/java/com/idfc/bootcamp/bookstore/BookstoreApplication.java
+++ src/main/java/com/idfc/bootcamp/bookstore/BookstoreApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @ComponentScan("com.idfc*")
 @EntityScan("com.idfc*")
+@CrossOrigin("*")
 public class BookstoreApplication {
 
 	public static void main(String[] args) {
--- src/test/java/com/idfc/bootcamp/bookstore/controller/BookControllerTest.java
+++ src/test/java/com/idfc/bootcamp/bookstore/controller/BookControllerTest.java
@@ -78,4 +78,5 @@ public class BookControllerTest {
                 andExpect(content().string("No data found"));
     }
 
+
 }
