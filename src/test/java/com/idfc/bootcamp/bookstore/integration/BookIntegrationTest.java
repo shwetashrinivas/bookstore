1e26025ac17a5367aada33be89a23fb01cef83c0
Badhmanaban Madhu <badhmanaban.madhu@idfcbank.com>
2024-02-13 12:52:04 +0530

    fix integration test, resolve dependency on the seed data

--- src/test/java/com/idfc/bootcamp/bookstore/integration/BookIntegrationTest.java
+++ src/test/java/com/idfc/bootcamp/bookstore/integration/BookIntegrationTest.java
@@ -58,6 +58,15 @@ public class BookIntegrationTest
     @Test
     @DisplayName("should return list of books")
     void shouldReturnListOfBooks() {
+        bookRepository.deleteAll();
+        Book book1 = new Book("Tale of two cities",  "Charles Dickens",  350.0,
+                "The novel tells the story of the French Doctor Manette, his 18-year-long imprisonment in the Bastille in Paris, and his release to live in London with his daughter Lucie whom he had never met. The story is set against the conditions that led up to the French Revolution and the Reign of Terror.",
+                "1586_f1601742134.png", 5.0, 3);
+        Book book2 = new Book("Little women",   "Louisa May Alcott", 450.0,
+                "It is set during and after the Civil War and tells the story of the March family, principally the four girls: Meg, Jo, Beth, and Amy. The girls struggle and grow as a family while their father is serving in the war and learn the value of hard work, self-sacrifice, and love.",
+                "1586_f1601742134.png", 5.0, 4);
+        bookRepository.saveAll(Arrays.asList(book1,book2));
+
         List<Book> bookList = restTemplate.exchange(
                 baseUrl + "/books",
                 HttpMethod.GET,
@@ -65,7 +74,7 @@ public class BookIntegrationTest
                 new ParameterizedTypeReference<List<Book>>() {
                 }).getBody();
 
-        assertEquals(5,bookList.size());
+        assertEquals(2,bookList.size());
     }
 
 }
