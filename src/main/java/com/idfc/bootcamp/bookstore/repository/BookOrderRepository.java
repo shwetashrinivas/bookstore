d23089cf26490b704c99c555ef7fa21129b37932
santosh.routray <santosh.routray@gmail.com>
2024-02-14 14:58:55 +0530

    Revert "WIP - Add order details and buy books endpoint"
    This reverts commit 8d834da6d8af51e478a954a7d0d36b97d1ec3148.

--- src/main/java/com/idfc/bootcamp/bookstore/repository/BookOrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.idfc.bootcamp.bookstore.repository;
-
-import com.idfc.bootcamp.bookstore.model.BookOrder;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookOrderRepository extends JpaRepository<BookOrder, Long> {
-}
--- src/main/java/com/idfc/bootcamp/bookstore/service/BookService.java
+++ src/main/java/com/idfc/bootcamp/bookstore/service/BookService.java
@@ -1,14 +1,10 @@
 package com.idfc.bootcamp.bookstore.service;
 
 import com.idfc.bootcamp.bookstore.model.Book;
-import com.idfc.bootcamp.bookstore.model.BookOrder;
-import com.idfc.bootcamp.bookstore.model.OrderItem;
-import com.idfc.bootcamp.bookstore.repository.BookOrderRepository;
 import com.idfc.bootcamp.bookstore.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,7 +12,6 @@ import java.util.Optional;
 public class BookService {
 
     BookRepository bookRepository;
-    BookOrderRepository bookOrderRepository;
 
     @Autowired
     public BookService(BookRepository bookRepository){
@@ -32,24 +27,5 @@ public class BookService {
 
     public Optional<Book> getById(Long bookId) {
         return bookRepository.findById(bookId);
-    }
-
-    public BookOrder buyBooks(List<Long> bookIds) {
-        BookOrder bookOrder = new BookOrder();
-        List<OrderItem> orderItems = new ArrayList<>();
-        for (Long bookId : bookIds) {
-            Book book = bookRepository.findById(bookId).orElseThrow(() -> new IllegalArgumentException("Book with id " + bookId + " not found"));
-            OrderItem orderItem = new OrderItem();
-            orderItem.setBook(book);
-            orderItem.setQuantity(1);
-            orderItem.setOrder(bookOrder);
-            orderItems.add(orderItem);
-        }
-        bookOrder.setOrderItems(orderItems);
-        return bookOrderRepository.save(bookOrder);
-    }
-
-    public List<BookOrder> getAllOrders() {
-        return bookOrderRepository.findAll();
     }
 }
